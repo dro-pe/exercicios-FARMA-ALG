b0cc8030c6df5a92a08a16ef1bebe121f1c7c746
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2022-09-03 21:44:15 -0300

    add ex-4 from 8th list (definitive)

--- lista-08/04/ex-4.pas
+++ lista-08/04/ex-4.pas
@@ -1,9 +1,6 @@
 program subsequencias;
-const MIN = 1; MAX = 100
+const MIN = 1; MAX = 100;
 type vetor_i = array[MIN..MAX] of longint;
-
-var v                           : vetor;
-    n, pos, tamanho_subsequencia: longint;
 
 procedure le_vetor(var w: vetor_i;
                    n    : longint);
@@ -14,17 +11,38 @@ begin
 end;
 
 function tem_subsequencias_iguais(var w     : vetor_i;
-                                  n, tam_sub: longint): longint;
+                                  tam_w, tam_sub: longint): longint;
+var i, j, k, cont, fim, ultima_atribuicao: longint;
+var sub                                  : vetor_i;
 begin
-    
-end;
-(*
-recebe uma subsequencia "v" que em tamanho "n" e procura por subsequencias
-iguais de tamanho "tam_sub". A funcao devolve zero se nao encontrou
-subsequencias iguais ou devolve a posicao do inicio da primeira subsequencia
-que encontrou.
-*)
+    tem_subsequencias_iguais := 0;
+    cont := 0;
+    fim := tam_w - tam_sub + 1;
+    ultima_atribuicao := fim - tam_sub;
 
+    while (tem_subsequencias_iguais = 0) and (cont < ultima_atribuicao) do
+    begin
+        for i := 1 to tam_sub do
+            sub[i] := w[i + cont];
+
+        j := tam_sub;
+        while (tem_subsequencias_iguais = 0) and (j < fim) do
+        begin
+            k := 1;
+            while (k <= tam_sub) and (sub[k] = w[k + j + cont]) do
+                k := k + 1;
+            if k > tam_sub then
+                tem_subsequencias_iguais := 1 + cont;
+            
+            j := j + 1;
+        end;
+        
+        cont := cont + 1;
+    end;  
+end;
+
+var v                           : vetor_i;
+    n, pos, tamanho_subsequencia: longint;
 begin
     read(n);
     le_vetor(v,n); 
@@ -37,7 +55,7 @@ begin
         tamanho_subsequencia := tamanho_subsequencia - 1;
     end;
     if pos > 0 then
-        writeln (pos,' ',tamanho_subsequencia+1)
+        writeln (pos,' ',tamanho_subsequencia + 1)
     else
         writeln ('nenhuma');
 end.
